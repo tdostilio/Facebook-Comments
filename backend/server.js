f4f7cddb2a7e6664914cd0156c35648bbef6a4f0
Tom Dostilio <tdostilio@gmail.com>
2018-12-07 13:16:58 -0500

    adds update and ddelete operations and timestamps

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,36 @@ router.post("/comments", (req, res) => {
   });
 });
 
+router.put("/comments/:commentId", (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: "No comment id provided" });
+  }
+  Comment.findById(commentId, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    const { author, text } = req.body;
+    if (author) comment.author = author;
+    if (text) comment.text = text;
+    comment.save(error => {
+      if (error) return res.json({ success: false, error });
+      return res.json({ success: true });
+    });
+  });
+});
+
+router.delete("/comments/:commentId", (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: "No comment id provided" });
+  }
+  Comment.remove({ _id: commentId }, (error, comment) => {
+    if (error) {
+      return res.json({ success: false, error });
+    }
+    return res.json({ success: true });
+  });
+});
+
 app.use("/api", router);
 
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
